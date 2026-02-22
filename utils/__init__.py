09925079150ce92f8688ce047efc9ddba1d4a048
abttnewacc-droid <abttnewacc@gmail.com>
2026-02-22 17:42:22 +0500

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,4 +1,17 @@
 from .validators import URLValidator
-from .helpers import format_duration, format_filesize, clean_filename
+from .helpers import (
+    format_duration,
+    format_filesize,
+    clean_filename,
+    extract_urls_from_text,
+    safe_delete_file,
+)
 
-__all__ = ['URLValidator', 'format_duration', 'format_filesize', 'clean_filename']
+__all__ = (
+    "URLValidator",
+    "format_duration",
+    "format_filesize",
+    "clean_filename",
+    "extract_urls_from_text",
+    "safe_delete_file",
+)
