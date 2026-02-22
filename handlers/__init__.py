03e2bfb9d67e227b742556c14cf198971627d806
abttnewacc-droid <abttnewacc@gmail.com>
2026-02-22 17:51:28 +0500

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,13 +1,11 @@
-from .init import (
-	start_register,
-	media_register,
-	audio_register,
-	recognition_register,
-)
+from .start import register as start_register
+from .media import register as media_register
+from .audio import register as audio_register
+from .recognition import register as recognition_register
 
 __all__ = [
-	'start_register',
-	'media_register',
-	'audio_register',
-	'recognition_register',
+    "start_register",
+    "media_register",
+    "audio_register",
+    "recognition_register",
 ]
